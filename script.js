73872dc25a75d51f27d93cf2ff68e4363eba311d
Aleksey Rozin <89986352+AlekseyRozin@users.noreply.github.com>
2022-11-10 20:36:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function toggleBurger() {
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+    console.log("sas");
+  }
